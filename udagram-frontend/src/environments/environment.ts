8abd6ce4a138f8aed1659d56ccadde7436e663db
Trang Huynh <trang.huynh@amplicade.com>
2022-03-20 13:14:04 +0700

    Update environment.ts file - Replace the keyword localhost in the http://localhost:8080/api/v0 string with the External-IP of the reverseproxy deployment

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a439c1d60a3124d4d84c66ba7c788b3e-1701475871.us-east-1.elb.amazonaws.com/api/v0'
 };
 
 /*
