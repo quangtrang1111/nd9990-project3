626a8faf4772b58152418c23d60a57b44be719c0
Trang Huynh <trang.huynh@amplicade.com>
2022-03-20 15:11:56 +0700

    Add kubectl logs screenshots

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=password1
-export POSTGRES_HOST=udagram2.csixzufepzrt.us-west-2.rds.amazonaws.com
-export POSTGRES_DB=udagram
-export AWS_BUCKET=arn:aws:s3:::udaconnect
-export AWS_REGION=us-west-2
-export AWS_PROFILE=test
+export POSTGRES_USERNAME=THE_POSTGRES_USERNAME
+export POSTGRES_PASSWORD=THE_POSTGRES_PASSWORD
+export POSTGRES_HOST=nd9990-project3.cleknghu6t7i.ap-southeast-1.rds.amazonaws.com
+export POSTGRES_DB=eks
+export AWS_BUCKET=nd9990-project3
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
